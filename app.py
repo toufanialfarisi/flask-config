7d87c78cb051f2a6babf20719a273706e5b567b6
toufani1515 <toufani1515@gmail.com>
2019-10-27 18:20:14 +0800

    fix

--- app.py
+++ app.py
@@ -2,8 +2,14 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ["APP_SETTINGS"])
+# print("ENVIRONMENT : ", os.environ["APP_SETTINGS"])
+
+
+@app.route("/")
+def index():
+    return "<h1>Hallo dunia</h1>"
+
 
 if __name__ == "__main__":
     app.run()
